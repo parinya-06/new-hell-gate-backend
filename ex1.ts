a78bedc87abe1ec2e670c188a2551b50b2238836
parinya-06 <parinya.ru.61@ubu.ac.th>
2022-06-29 12:35:50 +0700

    update func ex1

--- ex1.ts
+++ ex1.ts
@@ -1,15 +1,46 @@
 export class Utils {
-  public static findMax(...args: number[]): number {
-    return Infinity
-  }
+    public static findMax(...args: number[]): number {
+        let maxNumber: number;
+        args.forEach((number: number) => {
+            if (maxNumber != undefined) {
+                if (number >= maxNumber) {
+                    maxNumber = number
+                }
+            } else {
+                maxNumber = number
+            }
+        })
+        console.log('max', '=', maxNumber)
+        return maxNumber;
+    }
 
-  public static findMin(...args: number[]): number {
-    return Infinity
-  }
+    public static findMin(...args: number[]): number {
+        let minNumber: number;
+        args.forEach((number: number) => {
+            if (!minNumber) {
+                minNumber = number
+            } else {
+                if (number <= minNumber) {
+                    minNumber = number
+                }
+            }
+        })
+        console.log('min', '=', minNumber)
+        return minNumber;
+    }
 
-  public static reformatData(x: any): Record<string, any> {
-    return {}
-  }
+    public static reformatData(x: any): Record<string, any> {
+        const result = x.reduce((previousValue, currentValue) => {
+            return (
+                {
+                    ...previousValue,
+                    [currentValue.role]: (previousValue[currentValue.role] || []).concat({nickname: currentValue.name})
+                }
+            )
+        }, {})
+        console.log('result', '=', result)
+        return result;
+    }
 }
 
-export default Utils
+export default Utils;
